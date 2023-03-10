edb4c0fbb1d0eb2362a92188b17eb83791403efb
Victor Gomes <cartoonbr495@gmail.com>
2023-03-09 22:56:16 -0400

    Validando entrada de dados para o registro...

--- src/Express/Routes/authentication/register/index.ts
+++ src/Express/Routes/authentication/register/index.ts
@@ -1,12 +1,18 @@
 import ReturnResponse from "../../../Response";
-import { z } from "zod";
 import express from "express";
+import { z } from "zod";
+import { userAccountSchema } from "../../../../Schemas/user";
+
 export default async function register(
   req: express.Request,
   res: express.Response
 ) {
   try {
-    const user = req.body;
+    const Body = userAccountSchema.safeParse(req.body);
+    if (!Body.success)
+      return res
+        .status(406)
+        .json(ReturnResponse(true, Body.error.errors[0].message));
   } catch (e) {
     return res.status(400).json(ReturnResponse(true, "Confira seu payload."));
   }
--- src/Schemas/user.ts
+++ src/Schemas/user.ts
@@ -1,12 +1,33 @@
 import { z } from "zod";
 
 export const userAccountSchema = z.object({
-  firstName: z.string(),
-  role: z.union([
-    z.literal("administrador"),
-    z.literal("financeiro"),
-    z.literal("operador"),
-  ]),
-  password: z.string(),
-  phone: z.number().min(9).max(9),
+  firstName: z.string({
+    invalid_type_error: "Confira seu nome...",
+    required_error: "Confira seu nome!",
+  }),
+  role: z.union(
+    [
+      z.literal("administrador"),
+      z.literal("financeiro"),
+      z.literal("operador"),
+    ],
+    {
+      required_error: "Confira seu cargo!",
+      invalid_type_error: "Opa, confira seu cargo!",
+    }
+  ),
+  password: z
+    .string({
+      invalid_type_error: "Confira sua senha...",
+      required_error: "Confira sua senha!",
+    })
+    .min(6)
+    .max(48),
+  phone: z
+    .number({
+      invalid_type_error: "Apenas numeros para o telefone...",
+      required_error: "Confira o numero de telefone...",
+    })
+    .min(9)
+    .max(9),
 });
