e677d6c7c6155214e08fab0511d28716221a8c80
Victor Gomes <cartoonbr495@gmail.com>
2023-03-09 20:57:30 -0400

    Variavel de ambiente configurada

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Express from "./Express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
-Express.listen(3000, () => {
-  console.log(1);
+Express.listen(process.env.WEB_PORT, () => {
+  console.log("WebService " + process.env.WEB_PORT);
 });
