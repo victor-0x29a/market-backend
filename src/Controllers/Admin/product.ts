6a2a127c7d1c561a380cef5df1917c3324c16520
victor-0x29a <cartoonbr495@gmail.com>
2023-05-01 19:09:17 -0700

    update: add stock to products controller

--- src/Controllers/Admin/product.ts
+++ src/Controllers/Admin/product.ts
@@ -9,10 +9,24 @@ import Product from '../../Database/models/product'
 
 import { ProductNewSchema } from '../../Schemas/product'
 
+import { EditProductSchema } from '../../Schemas/editproduct'
+
 import { productFace } from '../../types/product.options'
 
+import { ID, Numerico } from '../../Schemas/id'
+
+import { MethodsStock } from '../../Schemas/methodsstock'
+
+import NormalStock from "../../Database/MongoDB/collections/sumStock";
+
+import DelStock from "../../Database/MongoDB/collections/removestock";
+
+import BreakDownStock from "../../Database/MongoDB/collections/breakdown"
+
+import { decodedData } from '../../types/decodedData.options'
+
 const ProductController = {
-  create: async (body: productFace) => {
+  create: async (body: productFace): Promise<response> => {
 
     const bodyValidation = ProductNewSchema.safeParse(body)
 
@@ -62,6 +76,270 @@ const ProductController = {
       }
     })
 
+  },
+  edit: async (body: productFace): Promise<response> => {
+
+    const idValidation = ID.safeParse(body.id)
+    if (!idValidation.success) return {
+      error: true, message: "Confira o ID!",
+      statusCode: 406
+    }
+
+    const bodyValidation = EditProductSchema.safeParse(body)
+
+    if (!bodyValidation.success) return {
+      error: true,
+      message: `Confira o campo ${bodyValidation.error.errors[0].path[0]}`,
+      statusCode: 406
+    }
+
+    const findProduct: productFace | null = await Product.findOne({
+      where: { id: body.id },
+    })
+
+    if (!findProduct) return {
+      error: true,
+      message: `Produto inexistente.`,
+      statusCode: 404
+    }
+
+    let supplier: supplierFace | null = await Supplier.findOne({
+      where: { id: Number(body.enterprise) },
+    });
+
+    if (!supplier) return {
+      error: true,
+      message: `Confira a empresa, pois esta empresa não foi encontrada!`,
+      statusCode: 404
+    }
+
+    return await Product.update(
+      {
+        name: body.name,
+        price: body.price,
+        enterprise: body.enterprise,
+        replacement: body.replacement,
+      },
+      {
+        where: {
+          id: Number(body.id),
+        },
+      }
+    ).then(() => {
+      return {
+        error: false,
+        message: `Produto atualizado!`,
+        statusCode: 204
+      }
+    }).catch((err) => {
+      return {
+        error: true,
+        message: `Houve um erro não esperado.`,
+        statusCode: 400
+      }
+    })
+
+  },
+  stock: async (method: string, id: number, valor: number, usuario: decodedData): Promise<response> => {
+
+    const idValidation = ID.safeParse(id)
+
+    if (!idValidation.success) return {
+      error: true, message: "Confira o ID.",
+      statusCode: 406
+    }
+
+    const valueValidation = Numerico.safeParse(valor)
+
+    if (!valueValidation.success) return {
+      error: true, message: "Confira o VALOR indicado.",
+      statusCode: 406
+    }
+
+    const methodValidation = MethodsStock.safeParse(method)
+
+    if (!methodValidation.success) return {
+      error: true,
+      message: `O método de controle de estoque não foi encontrado!`,
+      statusCode: 404
+    }
+
+    const findProduct: productFace | null = await Product.findOne({
+      where: { id: id },
+    })
+
+    if (!findProduct) return {
+      error: true,
+      message: `Produto inexistente.`,
+      statusCode: 404
+    }
+
+    switch (method) {
+      case "add":
+        const estoquePrevioAdicionar: number = findProduct.stock! + Number(valor);
+        return await Product.update(
+          {
+            stock: estoquePrevioAdicionar,
+          },
+          {
+            where: {
+              id: Number(id),
+            },
+          }
+        )
+          .then(async () => {
+            return await NormalStock.create({
+              product: Number(id),
+              user: usuario.id,
+              total: Number(valor),
+            })
+              .then(() => {
+                return {
+                  error: false,
+                  message: `Estoque atualizado.`,
+                  statusCode: 204
+                }
+              })
+              .catch(async (err: Error) => {
+                await Product.update({
+                  stock: findProduct.stock,
+                },
+                  {
+                    where: {
+                      id: Number(id),
+                    },
+                  })
+                return {
+                  error: true,
+                  message: `Houve um erro interno que causou um conflito, mas já está sendo resolvido.`,
+                  statusCode: 500
+                }
+              });
+          })
+          .catch((err: Error) => {
+            return {
+              error: true,
+              message: `Houve um erro interno.`,
+              statusCode: 400
+            }
+          });
+      case "sub":
+        const estoquePrevioRemover: number = findProduct.stock! - Number(valor);
+
+        if (estoquePrevioRemover < 0) return {
+          error: true,
+          message: `Valor negativo não é possível registrar!.`,
+          statusCode: 404
+        }
+
+        return await Product.update(
+          {
+            stock: estoquePrevioRemover,
+          },
+          {
+            where: {
+              id: Number(id),
+            },
+          }
+        )
+          .then(async () => {
+            return await DelStock.create({
+              product: Number(id),
+              user: usuario.id,
+              total: Number(valor),
+            })
+              .then(() => {
+                return {
+                  error: false,
+                  message: `Estoque atualizado.`,
+                  statusCode: 204
+                }
+              })
+              .catch(async (err: Error) => {
+                await Product.update({
+                  stock: findProduct.stock,
+                },
+                  {
+                    where: {
+                      id: Number(id),
+                    },
+                  })
+                return {
+                  error: true,
+                  message: `Houve um erro interno que causou um conflito, mas já está sendo resolvido.`,
+                  statusCode: 500
+                }
+              });
+          })
+          .catch((err: Error) => {
+            return {
+              error: true,
+              message: `Houve um erro interno.`,
+              statusCode: 400
+            }
+          });
+      case "break":
+        let estoquePrevioBreakdown: number = findProduct.breakdownStock! - Number(valor);
+
+        if (estoquePrevioBreakdown < 0) return {
+          error: true,
+          message: `Valor negativo não é possível registrar.`,
+          statusCode: 404
+        }
+
+        return await Product.update(
+          {
+            stock: estoquePrevioBreakdown,
+          },
+          {
+            where: {
+              id: Number(id),
+            },
+          }
+        )
+          .then(async () => {
+            return await BreakDownStock.create({
+              product: Number(id),
+              user: usuario.id,
+              total: Number(valor),
+            })
+              .then(() => {
+                return {
+                  error: false,
+                  message: `Estoque atualizado.`,
+                  statusCode: 204
+                }
+              })
+              .catch(async (err: Error) => {
+                await Product.update({
+                  stock: findProduct.stock,
+                },
+                  {
+                    where: {
+                      id: Number(id),
+                    },
+                  })
+                return {
+                  error: true,
+                  message: `Houve um erro interno que causou um conflito, mas já está sendo resolvido.`,
+                  statusCode: 500
+                }
+              });
+          })
+          .catch((err: Error) => {
+            return {
+              error: true,
+              message: `Houve um erro interno.`,
+              statusCode: 400
+            }
+          });
+      default:
+        return {
+          error: true,
+          message: `O método de controle de estoque não foi encontrado!`,
+          statusCode: 404
+        }
+    }
   }
 }
 
--- src/Express/Routes/admin/product/stock/index.ts
+++ src/Express/Routes/admin/product/stock/index.ts
@@ -10,163 +10,23 @@ import jwtDecode from "jwt-decode";
 import NormalStock from "../../../../../Database/MongoDB/collections/sumStock";
 import DelStock from "../../../../../Database/MongoDB/collections/removestock";
 
+import ProductController from '../../../../../Controllers/Admin/product'
+
 export default async function ProductStockRamAdmin(
   Req: express.Request,
   Res: express.Response
 ) {
   try {
-    const id = ID.safeParse(Number(Req.query.id));
-    if (!id.success)
-      return Res.status(406).json(ReturnResponse(true, "Confira o ID!"));
-    const value = Numerico.safeParse(Number(Req.query.value));
-    if (!value.success)
-      return Res.status(406).json(ReturnResponse(true, "Confira o valor!"));
-    const method = MethodsStock.safeParse(Req.query.method);
-    if (!method.success)
-      return Res.status(406).json(ReturnResponse(true, "Confira o metodo!"));
-    const Produto: productFace | null = await Product.findOne({
-      where: {
-        id: Number(Req.query.id),
-      },
-    });
-    if (!Produto)
-      return Res.status(404).json(
-        ReturnResponse(true, "O produto nao foi encontrado, tente novamente!")
-      );
-    if (Req.query.method === "sub") {
-      let header: string = Req.headers["authorization"]!;
-      let user: decodedData = jwtDecode(header);
-      let estoquePrevioSub: number = Produto.stock! - Number(Req.query.value);
-      if (estoquePrevioSub < 0)
-        return Res.status(409).json(
-          ReturnResponse(
-            true,
-            "Nao foi possivel atualizar o estoque, pois o valor iria ficar negativo!"
-          )
-        );
-      await Product.update(
-        {
-          stock: Number(estoquePrevioSub),
-        },
-        {
-          where: {
-            id: Number(Req.query.id),
-          },
-        }
-      )
-        .then(async () => {
-          await DelStock.create({
-            product: Number(Req.query.id),
-            user: user.id,
-            total: Number(Req.query.value),
-          })
-            .then(() => {
-              return Res.status(204).json();
-            })
-            .catch((err) => {
-              return Res.status(200).json(
-                ReturnResponse(
-                  true,
-                  "Houve um erro no final, porem o estoque foi atualizado!"
-                )
-              );
-            });
-        })
-        .catch((err) => {
-          return Res.status(503).json(
-            ReturnResponse(true, "Houve um erro, tente novamente... ")
-          );
-        });
-    } else if (Req.query.method === "add") {
-      let header: string = Req.headers["authorization"]!;
-      let user: decodedData = jwtDecode(header);
-      let estoquePrevioAdd: number = Produto.stock! + Number(Req.query.value);
-      await Product.update(
-        {
-          stock: estoquePrevioAdd,
-        },
-        {
-          where: {
-            id: Number(Req.query.id),
-          },
-        }
-      )
-        .then(async () => {
-          await NormalStock.create({
-            product: Number(Req.query.id),
-            user: user.id,
-            total: Number(Req.query.value),
-          })
-            .then(() => {
-              return Res.status(204).json();
-            })
-            .catch((err) => {
-              return Res.status(200).json(
-                ReturnResponse(
-                  true,
-                  "Houve um erro no final, porem o estoque foi atualizado!"
-                )
-              );
-            });
-        })
-        .catch((err) => {
-          return Res.status(503).json(
-            ReturnResponse(true, "Houve um erro, tente novamente... ")
-          );
-        });
-    } else if (Req.query.method === "break") {
-      let estoquePrevio: number = Produto.stock! - Number(Req.query.value);
-      if (estoquePrevio < 0)
-        return Res.status(409).json(
-          ReturnResponse(
-            true,
-            "Nao foi possivel atualizar o estoque, pois o valor iria ficar negativo!"
-          )
-        );
-      let header: string = Req.headers["authorization"]!;
-      let user: decodedData = jwtDecode(header);
-      let breakDownStock: number | undefined = Produto.breakdownStock;
-      let finalValue: number = 0;
-      if (!breakDownStock) {
-        finalValue = Number(Req.query.value);
-      } else {
-        finalValue = Number(Req.query.value) + breakDownStock;
-      }
-      await Product.update(
-        {
-          stock: estoquePrevio,
-          breakdownStock: Number(finalValue),
-        },
-        {
-          where: {
-            id: Number(Req.query.id),
-          },
-        }
-      )
-        .then(async () => {
-          await BreakDownStock.create({
-            product: Number(Req.query.id),
-            user: user.id,
-            total: Number(Req.query.value),
-          })
-            .then(() => {
-              return Res.status(204).json();
-            })
-            .catch((err) => {
-              return Res.status(200).json(
-                ReturnResponse(
-                  true,
-                  "Houve um erro no final, porem o estoque foi atualizado!"
-                )
-              );
-            });
-        })
-        .catch((err) => {
-          return Res.status(503).json(
-            ReturnResponse(true, "Houve um erro, tente novamente... ")
-          );
-        });
-    }
+
+    const header = Req.headers["authorization"]
+    const usuario: decodedData = jwtDecode(header!)
+
+    await ProductController.stock(Req.query.method!.toString(), Number(Req.query.id), Number(Req.query.value), usuario).then((data) => {
+      return Res.status(data.statusCode).json(ReturnResponse(data.error, data.message))
+    }).catch((err) => {
+      return Res.status(err.statusCode).json(ReturnResponse(err.error, err.message))
+    })
+
   } catch (e) {
     return Res.status(503).json(
       ReturnResponse(true, "Volte mais tarde, houve um erro...")
